27bddaadb8d00d4b3b8c58b6b4a717f3d38800f8
faktorsmak <rob.zakrzewski@gmail.com>
2013-08-01 11:55:46 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MantaProto::Application.config.secret_key_base = 'c9d1c8d6068e1cefdd4a541891f424b4598bbb6d2ab949140aacf97889c7f41db1228b4e4f2feaea4316ee229bf1cd95c5110782a4c56d33b8c956febc1481dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MantaProto::Application.config.session_store :cookie_store, key: '_manta_proto_session'
